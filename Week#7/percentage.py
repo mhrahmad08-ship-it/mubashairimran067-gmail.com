ff6c13b6aca974e017f917324aeda78640d26d4b
Ahmad Bilal 2026(S)cys-41 <mhrahmad08@gmail.com>
2026-05-02 12:53:00 +0500

    Add files via upload

---
+++ Week#7/percentage.py
@@ -0,0 +1,5 @@
+obt_marks = int(input("Enter your marks: "))
+total_marks = int(input("Enter your total marks: "))
+
+percentage = (obt_marks / total_marks) * 100
+print("Your percentage is:", percentage)
---
+++ Week#7/task4.py
@@ -0,0 +1,13 @@
+#a = int(input("Enter a number: "))
+#b = int(input("Enter a number: "))
+
+#c = a/b
+#print("The result is:", c)
+
+#a = [1,"Ahmad Bilal",3]
+#prin
+
+
+a = 2
+b = 2.5
+sum = a + b
