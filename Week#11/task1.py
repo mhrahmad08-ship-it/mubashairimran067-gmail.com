b04f9a5639345b51c138e40772d0ed986a164c62
Ahmad Bilal 2026(S)cys-41 <mhrahmad08@gmail.com>
2026-05-15 22:47:55 +0500

    Add files via upload

---
+++ Week#11/task1.py
@@ -0,0 +1,87 @@
+# S = {1,2,3, 'Ahmad'}
+# print(s)
+
+# S = {}
+# print(type(s))
+
+# S = {}
+# print(S)
+
+# a = set([1, 2, 3, 4])
+# print(type(a))
+
+# S = {'CE', 'CS', 'CE', 'IT'}
+# S.add('AE')
+# S.discard('CS')
+# for i in S:
+#      if i == "CE":
+#          print("Real apart.")
+#      else:
+#          print(i)
+
+# S = {'CE', 'CS', 'CE', 'IT'}
+# S.remove('AE')
+
+# print(S)
+
+# S = {'CE', 'CS', 'CE', 'IT'}
+# S.pop
+# print(S)
+
+# S = {'CE', 'CS', 'CE', 'IT'}
+# S.clear()
+
+# print(type(S))
+
+# S1 = {'Ahmad', 'Ali', 'Adnan', 'Latif'}
+# S2 = {41, 45, 39, 42}
+# S3 = {19 , 20, 19, 19}
+
+# a = print(set.union(S1,S2,S3))
+# print(a)
+
+# S1 = {'Ahmad', 'Ali', 'Adnan', 'Latif'}
+# S2 = {41, 45, 39, 42}
+# S3 = {19 , 20, 19, 19}
+
+# S1.update(S2)
+# print(S1)
+# print(S2)
+
+# S1 = {'Ahmad', 'Ali', 'Adnan', 'Latif'}
+# S2 = {41, 45, 39, 42}
+
+# a = set.intersection(S1, S2)
+# print(a)
+
+# S1 = {'Ahmad', 'Ali', 'Adnan', 'Latif'}
+# S2 = {41, 45, 39, 42}
+# S3 = {19 , 20, 19, 19}
+
+# S1.update(S2, S3)
+# S1.intersection_update(S2)
+
+# print(S1)
+# print(S2)
+# print(S3)
+
+# S1 = {42, 41, 42, 19}
+# S2 = {41, 45, 39, 42}
+
+# S = S1.symmetric_difference_update(S2)
+
+# print(S1)
+# print(S2)
+# print(S)
+
+S1 = {1, 2, 3}
+S2 = {1, 2, 3}
+
+print(S2.issubset(S1)) 
+ 
+
+
+
+
+
+
